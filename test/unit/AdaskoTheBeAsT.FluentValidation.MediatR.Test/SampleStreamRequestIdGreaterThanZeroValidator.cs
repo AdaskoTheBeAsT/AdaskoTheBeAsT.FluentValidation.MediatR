1538c1a480c0ded2ef5d3bdaef174bce6eec1a73
Adam Pluciński <adaskothebeast@gmail.com>
2023-07-16 12:18:47 +0200

    - MediatR 12.1.1
    - FluentValidation 11.6.0

--- test/unit/AdaskoTheBeAsT.FluentValidation.MediatR.Test/SampleStreamRequestIdGreaterThanZeroValidator.cs
+++ test/unit/AdaskoTheBeAsT.FluentValidation.MediatR.Test/SampleStreamRequestIdGreaterThanZeroValidator.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 
 namespace AdaskoTheBeAsT.FluentValidation.MediatR.Test;
+
 #pragma warning disable CA1710 // Identifiers should have correct suffix
 public class SampleStreamRequestIdGreaterThanZeroValidator
     : AbstractValidator<SampleStreamRequest>
